218d99968b54232afe4c8913c12322001eafd84e
tdrobinson94 <tdrobinson94@gmail.com>
2016-08-09 15:23:22 -0400

    added accounts controller

--- database/migrations/1470422231014_User.js
+++ database/migrations/1470422231014_User.js
@@ -5,14 +5,7 @@ const Schema = use('Schema')
 class UserSchema extends Schema {
 
   up () {
-    this.create('users', (table) => {
-      table.increments()
-      table.string('username', 80).notNullable().unique()
-      table.string('email', 254).notNullable().unique()
-      table.string('password', 60).notNullable()
-      table.string('firstname')
-      table.string('lastname')
-      table.timestamps()
+    this.table('users', (table) => {
     })
   }
 
