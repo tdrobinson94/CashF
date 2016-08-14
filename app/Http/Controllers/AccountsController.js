558424150db9137c6a1798e8350cfb57c48babbf
tdrobinson94 <tdrobinson94@gmail.com>
2016-08-14 13:28:33 -0400

    limiting number of accounts to 1 for now

--- app/Http/Controllers/AccountsController.js
+++ app/Http/Controllers/AccountsController.js
@@ -9,7 +9,7 @@ class AccountsController {
     const input = request.only('user_id');
     console.log(input);
     // const newAccount = yield Account.findBy('user_id', input.user_id);
-    const accounts = yield Account.query().where('user_id', input.user_id).fetch();
+    const accounts = yield Account.query().where('user_id', input.user_id).limit(1).fetch();
     return response.json(accounts.toJSON());
   }
 
